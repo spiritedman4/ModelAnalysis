846c8f5bd1d09e5690eb039ca0d893c042c02d4e
mortalreflections <80842304+mortalreflections@users.noreply.github.com>
2022-11-09 17:24:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -33,84 +33,8 @@ db.init_app(app)
 app.register_blueprint(forms,url_prefix="")
 app.register_blueprint(models,url_prefix="")
 
-INPUT_PATH = os.path.join(os.path.dirname(__file__), "static")
-OUTPUT_PATH = os.path.join(os.path.dirname(__file__), "staticfiles")
+app.wsgi_app = WhiteNoise(app.wsgi_app, root="static/")
 
-WHITENOISE_MAX_AGE = 31536000 if not app.config["DEBUG"] else 0
-
-    # configure WhiteNoise
-    app.wsgi_app = WhiteNoise(
-        app.wsgi_app,
-        root=os.path.join(os.path.dirname(__file__), "staticfiles"),
-        prefix="assets/",
-        max_age=WHITENOISE_MAX_AGE,
-    )
-import os
-import gzip
-SKIP_COMPRESS_EXTENSIONS = [
-    # Images
-    ".jpg",
-    ".jpeg",
-    ".png",
-    ".gif",
-    ".webp",
-    # Compressed files
-    ".zip",
-    ".gz",
-    ".tgz",
-    ".bz2",
-    ".tbz",
-    ".xz",
-    ".br",
-    # Flash
-    ".swf",
-    ".flv",
-    # Fonts
-    ".woff",
-    ".woff2",
-]
-
-
-def remove_files(path):
-    print(f"Removing files from {path}")
-    for filename in os.listdir(path):
-        file_path = os.path.join(path, filename)
-        try:
-            if os.path.isfile(file_path):
-                os.unlink(file_path)
-        except Exception as e:
-            print(e)
-
-
-def main():
-    # remove all files from "staticfiles"
-    remove_files(OUTPUT_PATH)
-
-    for dirpath, dirs, files in os.walk(INPUT_PATH):
-        for filename in files:
-            input_file = os.path.join(dirpath, filename)
-            with open(input_file, "rb") as f:
-                data = f.read()
-            # compress if file extension is not part of SKIP_COMPRESS_EXTENSIONS
-            name, ext = os.path.splitext(filename)
-            if ext not in SKIP_COMPRESS_EXTENSIONS:
-                # save compressed file to the "staticfiles" directory
-                compressed_output_file = os.path.join(OUTPUT_PATH, f"{filename}.gz")
-                print(f"\nCompressing {filename}")
-                print(f"Saving {filename}.gz")
-                output = gzip.open(compressed_output_file, "wb")
-                try:
-                    output.write(data)
-                finally:
-                    output.close()
-            else:
-                print(f"\nSkipping compression of {filename}")
-            # save original file to the "staticfiles" directory
-            output_file = os.path.join(OUTPUT_PATH, filename)
-            print(f"Saving {filename}")
-            with open(output_file, "wb") as f:
-                f.write(data)
-main()
 
 """-----------------------------------------------------------------------------------"""
 
